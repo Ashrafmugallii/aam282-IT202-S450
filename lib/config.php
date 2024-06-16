efedbeb771ba9786da240d9368fc265eb4f5ad65
aam282 <aam282@njit.edu>
2024-06-15 22:22:27 -0400

    added db info to config.php

--- lib/config.php
+++ lib/config.php
@@ -34,9 +34,9 @@ if(!$db_url || count($db_url) === 0){
     throw new Exception("Config parsing error, check the logs for further details");
 }
 else{
-    $dbhost   = $db_url["host"];
-    $dbuser = $db_url["user"];
-    $dbpass = $db_url["pass"];
+    $dbhost   = $db_url["db.ethereallab.app"];
+    $dbuser = $db_url["aam282"];
+    $dbpass = $db_url["KrWYeovopbbc"];
     $dbdatabase       = substr($db_url["path"],1);
 }
 ?>
