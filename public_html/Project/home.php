5704be60eb4047274fb9a1ce7833bd68ed9c9341
aam282 <aam282@njit.edu>
2024-07-14 16:35:44 -0400

    flash messages feature complete d

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,18 +3,18 @@ require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-/*
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- flash("Welcome, " . $_SESSION["user"]["email"];")
+if(is_logged_in()){
+    flash("Welcome, " . get_user_email());
 }
 else{
-  flash("You're not logged in");
+    flash("You're not logged in");
+}
+/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+ echo "Welcome, " . $_SESSION["user"]["email"]; 
+}
+else{
+  echo "You're not logged in";
 }*/
-if(is_logged_in()){
-   // flash("welcome, " . get_user_email(");
- }else{
-    flash("you are not logged in");
- }
-?>
 
-<?php require_once(__DIR__ . "/../../partials/flash.php");
+require(__DIR__."/../../partials/flash.php");
+?>
--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,97 +1,80 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
-        <input id="email" type="email" name="email" required />
+        <input type="email" name="email" required />
     </div>
     <div>
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
-    <input type="submit" value="login" />
+    <input type="submit" value="Login" />
 </form>
 <script>
     function validate(form) {
         //TODO 1: implement JavaScript validation
         //ensure it returns false for an error and true for success
+
         return true;
     }
 </script>
-
-
 <?php
- //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"])){
-    $email = se($_POST, "email", "", false);//$_POST["email"];
-    $password = se($_POST, "password", "", false); //$_POST["password"];
-    
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
 
-
-    //TODO 3:
+    //TODO 3
     $hasError = false;
-    if(empty($email)){
-        flash("must provide email <br> ");
+    if (empty($email)) {
+        flash("Email must not be empty");
         $hasError = true;
     }
-    
-     
-    //sanatize the email
-    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //sanitize
     $email = sanitize_email($email);
-  /*  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        flash("ENTER A VALID EMAIL! <br>");
-        $hasError = true;
-    }*/ 
-    if(!is_valid_email(($email))){
-        flash("ENTER A VALID EMAIL! <br>");
-        $hasError = true; 
-    }
-
-    if(empty($password)){
-        flash("must provide password <br> ");
+    //validate
+    if (!is_valid_email($email)) {
+        flash("Invalid email address");
         $hasError = true;
     }
-
-
-    if(strlen($password) < 8){
-        flash("password must be 8 at least 8 characters <br> ");
+    if (empty($password)) {
+        flash("password must not be empty");
         $hasError = true;
     }
-
-
-    if(!$hasError){
-       flash("Welcome in, ", $email);
-       //TODO 4: hash pass
-     
-       $db = getDB();
-       $stmt = $db->prepare("SELECT email, password from Users where email = :email");
-       try {
-           $r = $stmt->execute([":email" => $email]);
-           if ($r) {
-               $user = $stmt->fetch(PDO::FETCH_ASSOC);
-               if ($user) {
-                   $hash = $user["password"];
-                   unset($user["password"]);
-                   if (password_verify($password, $hash)) {
-                       //flash("Welcome $email";")
-                       $_SESSION["user"] = $user;
-                       die(header("Location: home.php"));
-                   } else {
-                       flash("Invalid password");
-                   }
-               } else {
-                   flash("Email not found");
-               }
-           }
-       } catch (Exception $e) {
-           flash("<pre>" . var_export($e, true) . "</pre>");
-       }
-
- }
+    if (strlen($password) < 8) {
+        flash("Password too short");
+        $hasError = true;
+    }
+    if (!$hasError) {
+        //TODO 4
+        $db = getDB();
+        $stmt = $db->prepare("SELECT email, password from Users where email = :email");
+        try {
+            $r = $stmt->execute([":email" => $email]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
+                    unset($user["password"]);
+                    if (password_verify($password, $hash)) {
+                        flash("Weclome $email");
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
+                    } else {
+                        flash("Invalid password");
+                    }
+                } else {
+                    flash("Email not found");
+                }
+            }
+        } catch (Exception $e) {
+            flash("<pre>" . var_export($e, true) . "</pre>");
+        }
+    }
 }
 ?>
-
-<?php require_once(__DIR__ . "/../../partials/flash.php");
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
