c4b44b10af0ec4bf22229fa4c559e1c6fea240ac
aam282 <aam282@njit.edu>
2024-07-15 20:10:03 -0400

    client side validation

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -14,35 +14,38 @@ require(__DIR__ . "/../../partials/nav.php");
 </form>
 <script>
     function validate(form) {
-    //TODO 1: implement JavaScript validation
-    //ensure it returns false for an error and true for success
+        //TODO 1: implement JavaScript validation
+        //ensure it returns false for an error and true for success
 
-    let email = form.email.value;
-    let password = form.password.value;
+        let email = form.email.value;
+        let username = form.username.value;
+        let password = form.password.value;
+        let confirm = form.confirm.value;
 
-    if (email.trim() === "") {
-        flash("Email/Username must not be empty", "danger");
-        return false;
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email)) {
+            flash("Invalid email address", "danger");
+            return false;
+        }
+
+        let usernamePattern = /^[a-zA-Z0-9_-]{3,16}$/;
+        if (!usernamePattern.test(username)) {
+            flash("Username must only contain 3-16 characters a-z, 0-9, _, or -", "danger");
+            return false;
+        }
+
+        if (password.length < 8) {
+            flash("Password must be at least 8 characters long", "danger");
+            return false;
+        }
+
+        if (password !== confirm) {
+            flash("Passwords must match", "danger");
+            return false;
+        }
+
+        return true;
     }
-
-    //TODO update clientside validation to check if it should
-    //valid email or username
-    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let usernamePattern = /^[a-zA-Z0-9_-]{3,16}$/;
-
-    if (!emailPattern.test(email) && !usernamePattern.test(email)) {
-        flash("Invalid email or username", "danger");
-        return false;
-    }
-
-    if (password.length < 8) {
-        flash("Password must be at least 8 characters long", "danger");
-        return false;
-    }
-
-    return true;
-}
-
 </script>
 <?php
 //TODO 2: add PHP Code
